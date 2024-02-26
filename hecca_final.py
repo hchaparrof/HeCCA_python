d6e28469061a034e3171aa702b03cb06d5954f4c
Fabio <hchaparrof@unal.edu.co>
2024-02-25 21:49:54 -0500

    añadida funcion main

--- hecca_final.py
+++ hecca_final.py
@@ -4,6 +4,9 @@ import ingreso_datos
 from estado_algoritmo import *
 
 
-if __name__ == '__main__':
+def main():
     instancia_algoritmo: EstadoAlgoritmo = ingreso_datos.iniciar_algoritmo()
     instancia_algoritmo.principal_funcion()
+
+if __name__ == '__main__':
+    main()
