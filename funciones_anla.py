aed906ca136799b0e9bb2fa7299573c4c81c3b55
Fabio <hchaparrof@unal.edu.co>
2024-03-22 21:35:59 -0500

    nombres y cosas así

--- funciones_anla.py
+++ funciones_anla.py
@@ -28,52 +28,49 @@ def calcular_q15(df_completo: pd.DataFrame) -> float:
     return (1 / (stdn_normal * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x_normal - mun_normal) / stdn_normal) ** 2)
   promedio_7_dias: pd.DataFrame = df_completo.rolling(7).mean()
   df_minimos_por_ano: pd.DataFrame = promedio_7_dias.groupby(promedio_7_dias.index.year)['cuenca-base'].min()
+  # iniciacion listas
+  vesn: list = [0]*len(df_minimos_por_ano['cuenca-base'])
+  vesln: list = [0] * len(df_minimos_por_ano['cuenca-base'])
+  vesp: list = [0] * len(df_minimos_por_ano['cuenca-base'])
+  vesg: list = [0]*len(df_minimos_por_ano['cuenca-base'])
+  vesw: list = [0]*len(df_minimos_por_ano['cuenca-base'])
+  # iniciacion valores
   mun, stdn = norm.fit(df_minimos_por_ano['cuenca-base'])
-  vesn: list = []
-  for x in df_minimos_por_ano['cuenca-base']:
-    n = normal_distr(x, mun, stdn)
-    vesn.append(n)
-  corn = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesn)[0, 1]
   pln = lognorm.fit(df_minimos_por_ano['cuenca-base'])
-  vesln = []
-  for x in df_minimos_por_ano['cuenca-base']:
-    n = lognormal_distr(x, pln[0], pln[2])
-    vesln.append(n)
-  corln = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesln)[0, 1]
   mug, beta = gumbel_r.fit(df_minimos_por_ano['cuenca-base'])
-  vesg = []
-  for x in df_minimos_por_ano['cuenca-base']:
-    n = gumball_distr(x, mug, beta)
-    vesg.append(n)
-  corg = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesg)[0, 1]
   a1, m, s = pearson3.fit(df_minimos_por_ano['cuenca-base'])
-  vesp = []
-  for x in df_minimos_por_ano['cuenca-base']:
-    n = pearson3.pdf(x, a1, m, s)
-    vesp.append(n)
-  corp = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesp)[0, 1]
-  # Weibull
   shape, loc, scale = weibull_min.fit(df_minimos_por_ano['cuenca-base'])
-  vesw = []
-  for x in df_minimos_por_ano['cuenca-base']:
+  for i, x in enumerate(df_minimos_por_ano['cuenca-base']):
+    n = normal_distr(x, mun, stdn)
+    vesn[i] = n
+    n = lognormal_distr(x, pln[0], pln[2])
+    vesln[i] = n
+    n = gumball_distr(x, mug, beta)
+    vesg[i] = n
     n = weibull_min.pdf(x, shape, loc, scale)
-    vesw.append(n)
-  corw = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesw)[0, 1]
+    vesw[i] = n
+    n = pearson3.pdf(x, a1, m, s)
+    vesp[i] = n
+  gumball_corr = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesg)[0, 1]
+  normal_corr = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesn)[0, 1]
+  pearson_corr = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesp)[0, 1]
+  lognorm_corr = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesln)[0, 1]
+  weibull_corr = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesw)[0, 1]
   # Sacar el mayor de las correlaciones y sacar en 10 años cuanto es el 7Q10
   tiempo_retorno = 10
-  pe = 1 - (1 / tiempo_retorno)
-  ajuste_seleccionado = mejor_ajuste(np.abs(corn), np.abs(corln), np.abs(corg), np.abs(corp), np.abs(corw))
+  prob_ret = 1 - (1 / tiempo_retorno)
+  ajuste_seleccionado = mejor_ajuste(np.abs(normal_corr), np.abs(lognorm_corr), np.abs(gumball_corr), np.abs(pearson_corr), np.abs(weibull_corr))
 
   if ajuste_seleccionado == 1:
-    return norm.ppf(pe, loc=mun, scale=stdn)  # 84.08149197181189
+    return norm.ppf(prob_ret, loc=mun, scale=stdn)  # 84.08149197181189
   elif ajuste_seleccionado == 2:
-    return lognorm.ppf(pe, pln[2], pln[2], pln[0])  # 84.08149197181189
+    return lognorm.ppf(prob_ret, pln[2], pln[2], pln[0])  # 84.08149197181189
   elif ajuste_seleccionado == 3:
-    return gumbel_r.ppf(pe, loc=mug, scale=beta)
+    return gumbel_r.ppf(prob_ret, loc=mug, scale=beta)
   elif ajuste_seleccionado == 4:
-    return pearson3.ppf(pe, a1, loc=m, scale=s)
+    return pearson3.ppf(prob_ret, a1, loc=m, scale=s)
   elif ajuste_seleccionado == 5:
-    return weibull_min.ppf(pe, shape, loc, scale)
+    return weibull_min.ppf(prob_ret, shape, loc, scale)
   else:
     return -1.0
 
